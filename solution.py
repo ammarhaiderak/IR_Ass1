9023a4a8d9995584e69bd57577181c94492b31b5
Ammar <ammarkhaliq@gmail.com>
2019-09-04 16:27:33 +0500

    uptill inverted index

--- solution.py
+++ solution.py
@@ -15,19 +15,41 @@ import re
 ps=PorterStemmer()
 
 
+term_id=0
+
+inverted_index={
+        }
+
+
+#inverted_index_item={
+#        'termid':None,
+#        'doclist':[
+#           {
+#            'doc':'docid1',
+#            'position':'position'                           
+#           },
+#           {
+#            'doc':'docid',
+#            'position':'position'                           
+#           },
+#           ...
+#           ]
+#       }
+
 
 class MyHTMLParser(HTMLParser):
-    stringofdata='empty'    
+    stringofdata=''    
         
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:",tag)
+    #def handle_starttag(self, tag, attrs):
+    #    print("Encountered a start tag:",tag)
 
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :",tag)
-
+    #def handle_endtag(self, tag):
+    #    print("Encountered an end tag :",tag)
+    
     def handle_data(self, data):
-        print("Encountered some data  :", data)
+        #print("Encountered some data  :", data)
         self.stringofdata=self.stringofdata+data
+    
     def getstringofdata(self):
         return self.stringofdata
     
@@ -57,55 +79,116 @@ def unique(list1):
 
 
 def nostopwords(list1): 
+    lines=[]
+    index=0
     with open('stoplist.txt','r') as f:
-        lines=f.readlines()
-    
+        for line in f:
+           # print(line,end='')
+            lines.insert(index,line)
+            index=index+1
+            
     list2=lines
     newlist=[]
     # traverse for all elements 
     for x in list1: 
-        if x not in list2: 
+        if x not in list2:
+            x.lower()
             newlist.append(x) 
     return newlist
 
 
 
+def maketxtfile(fileslist,filename):
+    with open(filename,'w') as f:
+        _id=0
+        for doc_name in fileslist:
+            f.write(doc_name+'\t'+str(_id)+'\n')
+            _id=_id+1
+    
+
+def writeterms(f_desc,terms_list):
+    global term_id
+    for term in terms_list:
+        f_desc.write(term+'\t'+str(term_id)+'\n')
+        term_id=term_id+1
+
+def addtoInvertedIndex(listofterms,docid):
+    global inverted_index
+    pos=0
+    for term in listofterms:
+        item={    
+                'docid':docid,
+                'position':pos
+             }
+
+        val=inverted_index.get(term)        
+        if val is None:
+            inverted_index[term]=[]
+        inverted_index[term].append(item)
+        pos=pos+1 
 
 
 
 
 listoffiles=None
 
+
+doc_directory = input("Enter name of the directory: ")  # Python 3
+print(doc_directory) 
+
 index=0
-for dirpath,dirnames,filenames in walk('testdir2'):
+for dirpath,dirnames,filenames in walk(doc_directory):
     #print(filenames)
     listoffiles=filenames
     index=index+1
 
-print(listoffiles)
-print(index)
+#print(listoffiles)
+#print(index)
+#python
 
-#comment on line 88
 
-#for filename in listoffiles:
-    #with open('testdir/'+filename,'r') as f:
-    with open('testdir2/'+listoffiles[0],'r') as f:
-        contents=f.read()
-        print(filename)
-        #print(contents)
-        parser.feed(contents)
-        text=parser.getstringofdata()
-        tokenized_list=re.findall('[A-Za-z]{1,}',text)
-        stemmed_list=stemthewords(tokenized_list)
-        unique_list=unique(stemmed_list)
-        no_stopwords=nostopwords(unique_list)
+
+#writing the file docs.txt
+maketxtfile(listoffiles,'docs.txt')
+
+doc_id=0
+
+with open('terms.txt','w') as f1:
+    for filename in listoffiles:
+        parser = MyHTMLParser()
+        with open(doc_directory+'/'+filename,'rb') as f:
+        #f=open(doc_directory+'/'+filename,'r') 
+        #with open('testdir2/'+listoffiles[0],'r') as f:
+            contents=f.read()
+            parser.feed(str(contents))
+            text=parser.getstringofdata()                   #fetches text
+            tokenized_list=re.findall('[A-Za-z]{1,}',text)  #regex tokenizing
+        #print(filename+'\n')
+        #print(tokenized_list)
+            stemmed_list=stemthewords(tokenized_list)       #stemming
+            unique_list=unique(stemmed_list)                #removing duplicates
+            finallist=nostopwords(unique_list)              #ignoring stopwords
+            writeterms(f1,finallist)            
+            addtoInvertedIndex(finallist,doc_id)
+        doc_id=doc_id+1
+        #f.close()
         
         
         
 
-print(no_stopwords)
-print(unique_list)
-print(stemmed_list)
-print(tokenized_list)
+
+#va=inverted_index.get('123')
+#if va is None:
+#    inverted_index['123']=[]
+#inverted_index['123'].append({'item1':1,'item2':2})
+#
+#
+#print(inverted_index)
+#print(inverted_index['is'])
+#
+#print(finallist)
+#print(unique_list)
+#print(stemmed_list)
+#print(tokenized_list)
 
 
